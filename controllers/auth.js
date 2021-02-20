df16279414fe3a9fb2e0f1a371991f7eacea0195
Sanday08 <sandip.job89@gmail.com>
2021-02-20 17:16:23 +0530

    add

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,6 +58,7 @@ exports.login = asyncHandler(async (req, res, next) => {
 exports.loginRetailer = asyncHandler(async (req, res, next) => {
 
   const { userName, password } = req.body;
+  console.log("gggggggggggggggg",username,password);
   //userName and password fields are required
   if (!userName&& !password) {
     return next(
