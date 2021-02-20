847be4a52aa461bec967439bc595e1f7d46d89dc
Sanday08 <sandip.job89@gmail.com>
2021-02-20 17:20:41 +0530

    add

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ const { protect,authorize } = require("../middleware/auth");
 const router = express.Router();
 // router.post("/", register);
 router.post("/login", login);
-router.route("/retailer/login").post(protect,authorize("retailer"), loginRetailer);
+router.route("/retailer/login").post(authorize("retailer"), loginRetailer);
 router.get("/logout", logout);
 router.route("/me").get( protect, authorize("Admin") , getMe);
 router.put( "updatedetails",protect, updateDetails);
