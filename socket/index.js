d1aea5025b047a8f0a131b89a129257bd9d7cc9a
Sanday08 <sandip.job89@gmail.com>
2021-02-24 16:12:53 +0530

    add

--- socket/index.js
+++ socket/index.js
@@ -5,12 +5,15 @@ const {getUserInfoBytoken} = require("./utils/users");
 const { customAlphabet } =require( 'nanoid')
 const nanoid = customAlphabet('1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ', 10)
 const immutable=require("object-path-immutable"); 
-const userBet ={};
+const userBet ={
+  1:{},3:{},5:{},6:{}
+};
   //retailerID:{1:{A:{10:2,5:4}},2:{A:{10:2,5:4}},3:{A:{10:2,5:4}},4:{A:{10:2,5:4}}}
 
 const allBet={
   1:{},3:{},5:{},6:{}
 }
+const lastMinutes=0;
 const liveRooms={};
 
 const adminBalance={ 1:0,3:0,5:0,6:0}
@@ -27,7 +30,7 @@ io.on("connection", socket => {
       console.log("Socket join call");
         let user = await getUserInfoBytoken(token);
         socket.emit("res", {
-          data: { user,currentTime: new Date().getTime().toString()},
+          data: { user,currentTime: new Date()},
           en: "join",
           status: 1,
         });
@@ -39,7 +42,7 @@ io.on("connection", socket => {
       console.log("Pila ye call karu..",series)
      
       console.log("**********",position);
-
+    
       adminBalance[series]= adminBalance[series]+(totalBetPoint-totalBetPoint*10/100)
       //Set user position to our functions      
       for (alfa of Object.keys(position)) {
@@ -68,6 +71,11 @@ io.on("connection", socket => {
     });
   });
   setInterval(() => {
-   
+   if(new Date().getHours()>8&&new Date().getHours()<22){
+      if(new Date().getMinutes()%15==0&& lastMinutes!=new Date().getMinutes()){
+        lastMinutes=new Date().getMinutes();
+
+      }
+   }
   }, 1000);
 
