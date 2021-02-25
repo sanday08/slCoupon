83b90d3a33ee40f7a310844d083b2ad5cf667aeb
Sanday08 <sandip.job89@gmail.com>
2021-02-25 14:48:03 +0530

    add

--- socket/index.js
+++ socket/index.js
@@ -46,6 +46,7 @@ io.on("connection", socket => {
     console.log("Pila ye call karu..", series)
         for (let alpha in position) {
           for (let number in position[alpha]) {
+              console.log(series);
               userBets = immutable.update(userBets, [retailerId, series, alpha, number], v => v ? v + position[alpha][number] : position[alpha][number])
               allBet=immutable.update(allBet,[series, alpha,number],v => v ? v + position[alpha][number] : position[alpha][number])
           }
