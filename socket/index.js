16754e555bf9baf24d2aa20c8eb8a1cfa688daa3
Sanday08 <sandip.job89@gmail.com>
2021-02-26 15:50:57 +0530

    add

--- socket/index.js
+++ socket/index.js
@@ -139,14 +139,16 @@ setInterval(async () => {
         await updateGameResult(i, winnerNumbersArray[i]);
       }
       console.log("Winner Array Is ", winnerNumbersArray);
-      if (ticketIdBase[series])
+      if (ticketIdBase)
         for (let series in winnerNumbers) {
           for (let alpha in winnerNumbers[series]) {
             let number = winnerNumbers[series][alpha];
-            for (t in ticketIdBase[series][alpha][number]) {
-              let price = ticketIdBase[series][alpha][number][t] * 90;
-              await winGamePay(ticketIdBase[t], price, t);
-            }
+            if (ticketIdBase[series][alpha])
+              if (ticketIdBase[series][alpha][number])
+                for (t in ticketIdBase[series][alpha][number]) {
+                  let price = ticketIdBase[series][alpha][number][t] * 90;
+                  await winGamePay(ticketIdBase[t], price, t);
+                }
           }
         }
 
