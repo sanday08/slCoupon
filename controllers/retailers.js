5777f2af83a20b502a0a83e8c93f5450c2bf6f39
Sanday08 <sandip.job89@gmail.com>
2021-03-09 16:21:55 +0530

    add

--- controllers/retailers.js
+++ controllers/retailers.js
@@ -42,9 +42,9 @@ exports.getBetHistroyReport = asyncHandler(async (req, res, next) => {
             }
         }, {
             $group: {
-                _id: 'DrDate',
+                _id: '$DrDate',
                 totalBetPonts: {
-                    $sum: 'betPoint'
+                    $sum: '$betPoint'
                 },
                 totalWon: {
                     $sum: 'won'
