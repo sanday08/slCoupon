900ca1e6c0b2f58d702d4fd2fa70c38e7798372f
Sanday08 <sandip.job89@gmail.com>
2021-03-11 15:20:03 +0530

    add

--- socket/utils/bet.js
+++ socket/utils/bet.js
@@ -42,7 +42,7 @@ async function updateGameResult(series, betResult) {
 
 async function deleteBet(retailerId, ticketId) {
   const betDetail = await Bet.findOne({ ticketId });
-  console.log("CancelBet Suthiye", betDetail, "lodu lasan", winPositions);
+  console.log("CancelBet Suthiye", betDetail, "lodu lasan", betDetail.winPositions);
   if (betDetail == null) {
     return "Ticket Not Exist ";
   }
