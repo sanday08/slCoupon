fe2e018f5f64df3b442d367b74c1d427743fcdcc
Sanday08 <sandip.job89@gmail.com>
2021-03-19 17:42:40 +0530

    add

--- controllers/retailers.js
+++ controllers/retailers.js
@@ -36,7 +36,7 @@ exports.get7Days = asyncHandler(async (req, res, next) => {
 //Access     Private/Retailers
 exports.getAnnouncement = asyncHandler(async (req, res, next) => {
     let announcement = await Announcement.findById("6039ea5b9ee94d505a90dd3e");
-    res.status(200).json({ success: true, data: announcement });
+    res.status(200).json({ success: true, data: announcement.announcement });
 });
 
 
